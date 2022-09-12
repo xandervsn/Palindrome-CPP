1640094233f1795183db7dd68016d8f648d37577
xandervsn <xandervsn@gmail.com>
2022-09-12 05:37:29 +0100

    Commit script.cpp (no lowercasing)

--- script.cpp
+++ script.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <regex>
+#include <cctype>
 
+using namespace std;
+
+int main(){
+  char input[81] = {"\0"};
+  
+  cout << "Enter a word: ";
+  cin >> input;
+
+  int length = strlen(input);
+  char forwards[length];
+  for(int i=0; i < length; i++){
+    forwards[i] = tolower(input[i]);
+  }
+
+    char backwards[length];
+  for(int i=length-1; i >= 0; i--){
+    backwards[i] = tolower(input[i]);
+  }
+
+  cout << forwards << ", " << backwards;
+
+  if(strcmp(forwards, backwards) == 0){
+    cout << "palindrome!";
+  }else{
+    cout << "not a palindrome.";
+  }
+
+
+
+  return 0;
+}
